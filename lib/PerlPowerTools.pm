9a03777f3b49d3edb1c6b419fcc32570ce649cab
brian d foy <brian.d.foy@gmail.com>
2014-09-12 08:16:47 -0400

    Create a main file with the name spelled out

---
+++ lib/PerlPowerTools.pm
@@ -0,0 +1,56 @@
+package PerlPowerTools;
+
+our $VERSION = '1.001';
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+PerlPowerTools - BSD utilities written in pure Perl
+
+=head1 SYNOPSIS
+
+Install the commands and use them just as you would the normal
+BSD utilities.
+
+=head1 DESCRIPTION
+
+
+Unix Reconstruction Project
+
+=head2 Utilities
+
+=over 4
+
+
+
+=back
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	https://github.com/briandfoy/ppt
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >> is the current maintainer, even
+though he write none of the utilities.
+
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2004-2014 brian d foy.  All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+
+
+
+__END__
