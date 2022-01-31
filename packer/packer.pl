8b9b8d018638c397db42d700e4c6a55a582b5804
kal247 <kal247@gmail.com>
2022-01-31 21:23:16 +0100

    packer.pl v1.21 + perldoc
    better packer.pl doc + added perldoc tool

--- packer/packer.pl
+++ packer/packer.pl
@@ -3,24 +3,46 @@
 # packer.pl - packer for PerlPowerTools
 #
 # This script builds :
-# 1. 'bin/perlpowertools'        -> helper script (calls unpacked tools from bin/*)
-# 2. 'packed/perlpowertools'     -> tools packed as a Perl script (needs external PAR module to run, platform-dependent)
-# 3. 'packed/perlpowertools.exe' -> tools packed as a Windows executable (standalone, no dependencies)
+# 1. bin/perlpowertools        -> helper script (calls unpacked tools from bin/*)
+# 2. packed/perlpowertools     -> tools packed as a Perl script (needs external PAR module to run, platform-dependent)
+# 3. packed/perlpowertools.exe -> tools packed as a Windows executable (standalone, no dependencies)
 #
-# The Perl script's shebang and second #line directive are replaced to improve portability,
-# provide meaningful error message to the end user and to avoid leaking data (full path of
-# perl and parl.pl running 'pp'). This couldn't be done for the Windows executable, because
-# it's built in a single step.
+# How to call the tools :
+#    bin/cat
+#    bin/perlpowertools cat
+#    packed/perlpowertools cat
 #
+# As BusyBox, if the packed file is renamed / hardlinked / symlinked as one of the tools,
+# it will behave as that tool automatically. The packed file MUST be called as 'perlpowertools'
+# or one of the tools, otherwise PAR won't know what to run.
+#
+# How to read documentation :
+#    bin/perldoc cat
+#    bin/perlpowertools perldoc cat
+#    packed/perlpowertools perldoc cat
+#
+# Implementation Notes :
+# - The Perl script's shebang and second #line directive are replaced to improve portability,
+#   provide meaningful error message to the end user and to avoid leaking data (full path of
+#   perl and parl.pl running 'pp'). This couldn't be done for the Windows executable, because
+#   it's built in a single step.
+# - For correctness, $0 should be set to $file (full path) instead of $tool (basename) and any
+#   display issue should be fixed in the tools themselves. Currently, we avoid very and ugly
+#   usage/warning/error messages (some tools use raw $0), but __FILE__ MUST be used to find
+#   the real path of the file (don't use $0 or modules using it).
+# - Executables produced by PAR::Packer are sometimes detected/deleted by antivirus software.
+#
+# TODO :
+# - find solution to pack perldoc's own POD
+# - have symbolic links on windows be tested by someone else (didn't work for me)
+#
+# 2022.01.31 v1.21 jul : better documentation
 # 2021.10.22 v1.20 jul : - fixed incorrect tools usage message by setting $0 
 #                        - in packed file and helper script : set shebang to #!/usr/bin/env perl
 #                        - in packed file : delete "#line 2" directive (line number and file name)
 #                        - helper script now in /bin, not deleted anymore, works when packed or not
 # 2021.05.31 v1.10 jul : fixed spaces in paths and "The command line is too long" (win32) 
 # 2021.05.22 v1.00 jul : initial
-#
-# TODO
-# - find solution for POD
 
 =begin metadata
 
@@ -165,7 +187,7 @@ my $tool = shift || '';
 die $usage if not grep { $tool eq $_ } @tools;
 
 my $file = defined $ENV{PAR_TEMP} ? "$ENV{PAR_TEMP}/inc/script/$tool" : dirname(abs_path($0)) . "/$tool";
-$0 = $tool; # for correct usage message 
+$0 = $tool; # for usage/warning/error messages
 my $return = do $file;
 die $@ if $@;
 die "$file: $!" unless defined $return;
