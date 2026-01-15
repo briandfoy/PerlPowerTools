92f79b99f1c6cf3e1e0090146e37cc970bd5d2d1
Zongnan HU <zongnan.hu@gmail.com>
2026-01-16 01:58:49 +0800

    restructure ar tests to improve extensibility
    - Automatically generate tests and the scripts used to generate
      test files from `@tests`.
    - Regenerated test archives using FreeBSD `ar`.
    - Reorganized test fixture files.

--- t/data/ar/gen_out_a.sh
+++ t/data/ar/gen_out_a.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+rm -rf out*.a
 
-rm -rf out*
-
-ar q out01.a a b c
+ar qc out01.a a b c
 
 ar rc out02.a a b c
 
-ar q out03.a a b c
-ar r out03.a ./another_a/a
+ar qc out03.a a b c
+ar r out03.a another/a
 
-ar q out04.a a b c
+ar qc out04.a a b c
 ar d out04.a a
 
-ar q out05.a a b c ./another_a/a
+ar qc out05.a a b c another/a
+
